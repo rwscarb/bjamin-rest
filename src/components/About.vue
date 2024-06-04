607d305833414f1ff948246a1a4a0b00b5cc6a62
Ryan Scarbery <ryan.scarbery@gmail.com>
2024-06-04 07:45:32 -0700

    add some links

--- src/components/About.vue
+++ src/components/About.vue
@@ -34,6 +34,79 @@
 
     </v-col>
   </v-row>
+  <v-row>
+    <v-col cols="12">
+      <h3>Links</h3>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col>
+      <v-card
+        title="Camping"
+        href="https://www.fresno.gov/parks/campfresno/"
+        image="https://bjamin.rest/img/links/camping.jpg"
+        height="200"
+        width="200"
+        />
+    </v-col>
+    <v-col>
+      <v-card
+        title="Biking"
+        style="color: black"
+        href="https://www.fs.usda.gov/recarea/sierra/recreation/hiking/recarea/?recid=45894&actid=51"
+        image="https://bjamin.rest/img/links/biking.jpg"
+        height="200"
+        width="200"
+      />
+    </v-col>
+    <v-col>
+      <v-card
+        title="Skateboarding"
+        style="color: black"
+        href="https://www.youtube.com/watch?v=oesiPltzs0I"
+        image="https://bjamin.rest/img/links/skateboarding.jpg"
+        height="200"
+        width="200"
+      />
+    </v-col>
+    <v-col>
+      <v-card
+        title="Slacklining"
+        href="https://en.wikipedia.org/wiki/Slacklining"
+        image="https://bjamin.rest/img/links/slack.jpg"
+        height="200"
+        width="200"
+      />
+    </v-col>
+    <v-col>
+      <v-card
+        title="Bouldering"
+        style="color: black"
+        href="https://en.wikipedia.org/wiki/Bouldering"
+        image="https://bjamin.rest/img/links/bouldering.jpeg"
+        height="200"
+        width="200"
+      />
+    </v-col>
+    <v-col>
+      <v-card
+        title="Led Zepplin"
+        href="https://www.youtube.com/watch?v=nhVfuacsLDw"
+        image="https://bjamin.rest/img/links/led-zepplin.jpg"
+        height="200"
+        width="200"
+      />
+    </v-col>
+    <v-col>
+      <v-card
+        title="Monty Python"
+        href="https://www.youtube.com/watch?v=RbTaP0_Galg"
+        image="https://bjamin.rest/img/links/monty-python.jpg"
+        height="200"
+        width="200"
+      />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
