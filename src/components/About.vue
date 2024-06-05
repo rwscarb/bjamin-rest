b80abbd97a94a2809fd684dfde47acd0919f8d49
Ryan Scarbery <ryan.scarbery@gmail.com>
2024-06-05 14:49:37 -0700

    fix public s3 prefix

--- src/components/About.vue
+++ src/components/About.vue
@@ -37,70 +37,77 @@
 </template>
 
 <script>
+import { getUrl } from "aws-amplify/storage";
+
 export default {
   name: "About",
   data() {
     return {
       selectedEvent: '',
+      timelineEvents: [],
     }
   },
-  computed: {
-    timelineEvents() {
-      return [
-        {
-          id: 'new_mexico',
-          title: 'New Mexico',
-          href: '#',
-          subtitle: 'Age 0-5',
-          image: 'https://bjamin.rest/img/flags/new-mexico.png',
-          icon: 'mdi-baby-carriage',
-          content: `Ben was born in New Mexico in 1983 by Mary and Lynn Johnson. He was born to an older sister of 6 years, Amanda.
+  async mounted() {
+    const events = [
+      {
+        id: 'new_mexico',
+        title: 'New Mexico',
+        href: '#',
+        subtitle: 'Age 0-5',
+        image: 'public/img/flags/new-mexico.png',
+        icon: 'mdi-baby-carriage',
+        content: `Ben was born in New Mexico in 1983 by Mary and Lynn Johnson. He was born to an older sister of 6 years, Amanda.
             And while New Mexico would only consist of a short period of his life,
             he would assimilate parts of the culture and would carry his <i>je ne sais quoi</i> brand to California.
             From the earliest years, he was a philosopher to his core. A true friend with a knack for mischief and an infectious laugh.
           `
-        },
-        {
-          id: 'california',
-          title: 'California',
-          href: '#',
-          subtitle: 'Age 6-27',
-          image: 'https://bjamin.rest/img/flags/california.png',
-          icon: 'mdi-account-school',
-          quotes: [
-            {
-              author: 'Ryan Scarbery',
-              content: `
+      },
+      {
+        id: 'california',
+        title: 'California',
+        href: '#',
+        subtitle: 'Age 6-27',
+        image: 'public/img/flags/california.png',
+        icon: 'mdi-account-school',
+        quotes: [
+          {
+            author: 'Ryan Scarbery',
+            content: `
                 Ben attended Eaton Elementary school in Fresno CA. He introduced himself to me on the first day of
                 kindergarten and we became friends for life. I can still hear his voice whisper "borrow" when it came time
                 to leave from a day of playing. It was my luck that Ben came into my life so early. Ben beamed
                 positivity and everyone saw it and wanted to be around it. He was there for so many of my firsts.
                 From church camp to stoner park, he'd usually have a frisbee in tow and would be down
                 to grab a cup of coffee and ask about life.`
-            },
-          ],
-        },
-        {
-          id: 'hawaii',
-          title: 'Hawaii',
-          href: '#',
-          subtitle: 'Age 27-39',
-          image: 'https://bjamin.rest/img/links/oahu.jpg',
-          icon: 'mdi-surfing',
-          quotes: [
-            {
-              author: 'Cam Prendergast',
-              content: `
+          },
+        ],
+      },
+      {
+        id: 'hawaii',
+        title: 'Hawaii',
+        href: '#',
+        subtitle: 'Age 27-39',
+        image: 'public/img/links/oahu.jpg',
+        icon: 'mdi-surfing',
+        quotes: [
+          {
+            author: 'Cam Prendergast',
+            content: `
                 <p>
                     Ben was a representative of the best of what our generation could be, and he only wanted the best
                     for everyone around him.
                 </p>
               `
-            },
-          ],
-        },
-      ]
-    }
+          },
+        ],
+      },
+    ];
+    await Promise.all(events.map(async event => {
+      event.image = (await getUrl({path: event.image})).url.href;
+    }));
+
+    console.log(events)
+    this.timelineEvents = events;
   }
 }
 </script>
